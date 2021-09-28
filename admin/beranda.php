6a22faad8b964b60134dda4c7bde1445a224fc8b
Iqbalmz <iqbalepaksolichin10@gmail.com>
2021-09-28 20:15:18 +0700

    menambah beranda

---
+++ admin/beranda.php
@@ -0,0 +1,10 @@
+<?php $page = "BERANDA";
+	include('inc/header.php');
+	include('inc/sidebar.php');
+?>
+
+<section class="main">
+	<h1>Selamat Datang, <?php echo $_SESSION['nama']; ?></h1>
+	
+</section>
+<?php include('inc/footer.php');?>
