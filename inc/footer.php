c4d895edf04e90a92574bcbb7422871712fbea6f
Rezabktr2303 <rezabaktiarpro@gmail.com>
2021-09-30 00:38:42 +0700

    update footer user

--- inc/footer.php
+++ inc/footer.php
@@ -6,48 +6,13 @@
 				
 				</section>
 				<hr>
-				<section id="links">
-					<div id="my-account">
-						<h4>AKUN SAYA</h4>
-						<ul>
-							<li><a href="account.php">Pengaturan Akun</a></li>
-							<li><a href="order_history.php">Daftar Pemesanan</a></li>
-							<li><a href="cart.php">Keranjang Belanja</a></li>
-						</ul>
-					</div>
-					<div id="info">
-						<h4>INFORMASI</h4>
-						<ul>
-							<li><a href="#">Persyaratan Penggunaan</a></li>
-							<li><a href="#">Kebijakan Privasi</a></li>
-							<li><a href="#">FAQ</a></li>
-						</ul>
-					</div>
-					<div id="extras">
-						<h4>LAINYA</h4>
-						<ul>
-							<li><a href="contact.php">Tentang</a></li>
-							<li><a href="contact.php">Hubungi</a></li>
-							<li><a href="blog.php">Blog</a></li>
-						</ul>
-					</div>
-					<div id="connect">
-						<h4>HUBUNGI KAMI</h4>
-						<ul>
-							<li class="twitter"><a href="http://www.twitter.com">Twitter</a></li>
-							<li class="facebook"><a href="http://www.facebook.com">Facebook</a></li>
-						</ul>
-					</div>
-				</section>
 				<section class="clearfix">
 					<div id="copyright">
 						<div id="logo">
 							<a href="index.php"><img src="img/logo.png"></a>
 						</div>
 						<div id="payments">
-							<h4>CARA PEMBAYARAN KAMI</h4>
-							<img src="img/payment-methods.gif" alt="Supported Payment Methods">
-							<span> Author: @iqbal_kbl</span>
+							<span> Author: @magang_codelaris</span>
 						</div>
 				</section>
 				
