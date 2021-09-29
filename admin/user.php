3600ac010ebc37545309df69fb82261ac6c709dd
Rezabktr2303 <rezabaktiarpro@gmail.com>
2021-09-29 20:07:56 +0700

    memperbaiki error user

--- admin/user.php
+++ admin/user.php
@@ -2,7 +2,7 @@
 	include('inc/header.php');	
 	include('inc/sidebar.php');
 ?>
-<section class="main" style="background-color: #8B4513;">
+<section class="main">
 	<h1>Manajemen User</h1>
 	<hr>
 	<?php 
@@ -10,31 +10,23 @@
 			echo "
 				<h3>Tambah Data</h3>
 <form name='tambah' action='?act=proses_tambah' method='post' enctype='multipart/form-data'>
+	<p><input type='number' name='NIS' placeholder='NIS' required></p>
 	<p><input type='text' name='nama' placeholder='Nama' required></p>
+	<p><input type='text' name='username' placeholder='username' required></p>
 	<p><input type='password' name='password' placeholder='Password' required></p>
-	<p><textarea name='alamat' cols='50' rows='10' placeholder='Alamat' required='true'></textarea></p>
 	<p><input type='text' name='no_telepon' placeholder='Telepon' id='telepon' onkeyup='format_number(this)' required></p>
-						<p><input type='file' name='gambar'></p>
+	<p>role: <select name='role'>
+	<option value='1'>admin</option>
+	<option value='2'>user</option>
+	</select>
+	</p>
 	<p><input type='submit' name='proses' value='Simpan' class='btn btn-danger'></p>
 </form>
 <hr>
 			";
 		}elseif(isset($_GET['act'])AND $_GET['act']=='proses_tambah'){
 			
-				if($_FILES['gambar']['error'] !=0){
-					$tambah = mysqli_query($connect, "INSERT into admin(username, password, alamat, no_telepon)VALUES('$_POST[nama]', '$_POST[password]', '$_POST[alamat]', '$_POST[no_telepon]')");
-					}else{
-						$tmp_file = $_FILES['gambar']['tmp_name'];
-						$filename = $_FILES['gambar']['name'];
-						$filetype = $_FILES['gambar']['type'];
-						$filesize = $_FILES['gambar']['size'];
-						$destination = 'img/'.$filename;
-
-						if(move_uploaded_file($tmp_file, $destination)){
-							$gambar = $filename;
-						}
-						$tambah = mysqli_query($connect, "INSERT into admin(username, password, alamat, no_telepon, foto)VALUES('$_POST[nama]', '$_POST[password]', '$_POST[alamat]', '$_POST[no_telepon]', '$gambar')");
-					}
+				$tambah = mysqli_query($connect, "INSERT into user(NIS, nama_user,telepon, username, password, role)VALUES('$_POST[NIS]', '$_POST[nama]', '$_POST[no_telepon]', '$_POST[username]',md5('$_POST[password]'),  '$_POST[role]')");
 			if($tambah){
 				echo "Data berhasil ditambahkan";
 			}else{
@@ -42,37 +34,27 @@
 			}
 			echo "<hr>";
 		}elseif(isset($_GET['act'])AND $_GET['act']=='edit'){
-			$isi = mysqli_fetch_array(mysqli_query($connect, "SELECT * from admin where id_admin = '$_GET[id]'"));
+			$isi = mysqli_fetch_array(mysqli_query($connect, "SELECT * from user where id_user = '$_GET[id]'"));
 			echo "
 				<h3>Edit Data</h3>
 <form name='edit' action='?act=proses_edit' method='post' enctype='multipart/form-data'>
-	<input type='hidden' name='id' value='$isi[id_admin]' required>
-	<p><input type='text' name='nama' value='$isi[username]' placeholder='Nama' required></p>
+	<input type='hidden' name='id' value='$isi[id_user]' required>
+	<p><input type='number' name='NIS' value='$isi[NIS]' placeholder='NIS' required></p>
+	<p><input type='text' name='nama' value='$isi[nama_user]' placeholder='Nama' required></p>
+	<p><input type='text' name='username' value='$isi[username]' placeholder='username' required></p>
 	<p><input type='password' name='password' value='$isi[password]' placeholder='Password' required></p>
-	<p><textarea name='alamat' cols='50' rows='10' placeholder='Alamat' required='true'>$isi[alamat]</textarea></p>
-	<p><input type='text' name='no_telepon' placeholder='Telepon' id='telepon' onkeyup='format_number(this)' value='$isi[no_telepon]' required></p>
-	<img src='img/$isi[foto]' alt='$isi[username]' height='400px' weight='200px'><br>
-					<input type='file'  name='gambar'>
+	<p><input type='text' name='no_telepon' placeholder='Telepon' id='telepon' value='$isi[telepon]' required></p>
+	<p>role: <select name='role'>
+	<option value='1'>admin</option>
+	<option value='2'>user</option>
+	</select>
+	</p>
 	<p><input type='submit' name='proses' value='Simpan' class='btn btn-danger' ></p>
 </form>
 <hr>
 			";
 		}elseif(isset($_GET['act'])AND $_GET['act']=='proses_edit'){
-			if($_FILES['gambar']['error'] !=0){
-						$edit = mysqli_query($connect, "UPDATE admin set username='$_POST[nama]', password='$_POST[password]', alamat='$_POST[alamat]', no_telepon='$_POST[no_telepon]' where id_admin='$_POST[id]'");
-					}else{
-						$tmp_file = $_FILES['gambar']['tmp_name'];
-						$filename = $_FILES['gambar']['name'];
-						$filetype = $_FILES['gambar']['type'];
-						$filesize = $_FILES['gambar']['size'];
-
-						$destination = 'img/' . $filename;
-
-						if(move_uploaded_file($tmp_file, $destination)){
-							$gambar = $filename;
-						}
-						$edit = mysqli_query($connect, "UPDATE admin set username='$_POST[nama]', password='$_POST[password]', alamat='$_POST[alamat]', no_telepon='$_POST[no_telepon]', foto='$gambar' where id_admin='$_POST[id]'");
-					}
+			$edit = mysqli_query($connect, "UPDATE user set username='$_POST[username]', NIS='$_POST[NIS]', password=md5('$_POST[password]'), nama_user='$_POST[nama]', telepon='$_POST[no_telepon]', role='$_POST[role]' where id_user='$_POST[id]'");
 			if($edit){
 				echo "Data berhasil di update";
 			}else{
@@ -80,7 +62,7 @@
 			}
 			echo "<hr>";
 		}elseif(isset($_GET['act'])AND $_GET['act']=='hapus'){
-			$hapus = mysqli_query($connect, "DELETE from admin where id_admin ='$_GET[id]'");
+			$hapus = mysqli_query($connect, "DELETE from user where id_user ='$_GET[id]'");
 
 			if($hapus){
 				echo "Data berhasil dihapus";
@@ -98,7 +80,6 @@
 			<th>ID</th>
 			<th>Nama</th>
 			<th>Password</th>
-			<th>Alamat</th>
 			<th>No telepon</th>
 			<th>Aksi</th>
 		</thead>
